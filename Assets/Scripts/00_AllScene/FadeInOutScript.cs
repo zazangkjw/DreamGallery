b7ffeabffa1f56fb96500ffa364a96467bf2a2b3
zazangkjw <zazangkjw@naver.com>
2024-04-27 00:09:32 +0900

    페이드인아웃 코드 deltatime 사용으로 수정 / 플레이어 공중 움직임 감소 기능 삭제

--- Assets/Scripts/00_AllScene/FadeInOutScript.cs
+++ Assets/Scripts/00_AllScene/FadeInOutScript.cs
@@ -1,41 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FadeInOutScript : MonoBehaviour
 {
-    WaitForSeconds wait = new WaitForSeconds(0.016f);
-
+    // 페이드 인
     public void FadeIn(RawImage rawImage)
     {
-        StartCoroutine(FadeInCoroutine(rawImage));
+        StartCoroutine(FadeInCoroutine(rawImage, 2));
     }
-
-    public void FadeOut(RawImage rawImage)
+    // 페이드 인(시간 입력)
+    public void FadeIn(RawImage rawImage, float time)
     {
-        StartCoroutine(FadeOutCoroutine(rawImage));
+        StartCoroutine(FadeInCoroutine(rawImage, time));
     }
-
-    // 페이드 인 코루틴
-    IEnumerator FadeInCoroutine(RawImage rawImage)
+    IEnumerator FadeInCoroutine(RawImage rawImage, float time)
     {
         rawImage.color = new Color(rawImage.color.r, rawImage.color.g, rawImage.color.b, 0f);
         while (rawImage.color.a < 1f)
         {
-            rawImage.color = new Color(rawImage.color.r, rawImage.color.g, rawImage.color.b, rawImage.color.a + (0.0111f));
-            yield return wait;
+            rawImage.color = new Color(rawImage.color.r, rawImage.color.g, rawImage.color.b, rawImage.color.a + ((1f / time) * Time.deltaTime));
+            yield return null;
         }
     }
 
-    // 페이드 아웃 코루틴
-    IEnumerator FadeOutCoroutine(RawImage rawImage)
+
+
+
+    // 페이드 아웃
+    public void FadeOut(RawImage rawImage)
+    {
+        StartCoroutine(FadeOutCoroutine(rawImage, 2));
+    }
+
+    // 페이드 아웃(시간 입력)
+    public void FadeOut(RawImage rawImage, float time)
+    {
+        StartCoroutine(FadeOutCoroutine(rawImage, time));
+    }
+    IEnumerator FadeOutCoroutine(RawImage rawImage, float time)
     {
         rawImage.color = new Color(rawImage.color.r, rawImage.color.g, rawImage.color.b, 1f);
         while (rawImage.color.a > 0f)
         {
-            rawImage.color = new Color(rawImage.color.r, rawImage.color.g, rawImage.color.b, rawImage.color.a - (0.0111f));
-            yield return wait;
+            rawImage.color = new Color(rawImage.color.r, rawImage.color.g, rawImage.color.b, rawImage.color.a - ((1f / time) * Time.deltaTime));
+            yield return null;
         }
     }
 }
