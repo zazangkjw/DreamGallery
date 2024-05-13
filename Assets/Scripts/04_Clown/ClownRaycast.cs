d644d57c22c1e148ac7acca2aca7882ec4f7ae9e
zazangkjw <zazangkjw@naver.com>
2024-05-14 03:53:13 +0900

    안전 구역 엘리베이터 추가 / 엘리베이터 기능 구현 / 버튼 외곽선 기능 구현

---
+++ Assets/Scripts/04_Clown/ClownRaycast.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClownRaycast : MonoBehaviour
+{
+    public RaycastHit hitInfo;
+    public GameObject hitObject;
+    public GameObject preObject;
+    public TextMeshProUGUI mouseText;
+    public PutDialogScript putDialogScript;
+
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    GameObject Objects;
+
+    [SerializeField]
+    Animator[] elevatorAnims;
+
+    [SerializeField]
+    GameObject[] elevatorButtons;
+
+    [SerializeField]
+    GameObject elevator_Point_Player;
+
+    GameObject button;
+
+
+
+
+    void Start()
+    {
+        // 처음에 열려있어야 하는 엘리베이터 문들
+        elevatorAnims[0].Play("Open");
+        elevatorAnims[1].Play("Open");
+        elevatorAnims[3].Play("Open");
+    }
+
+    void Update()
+    {
+        ShootRaycast();
+    }
+
+    // 카메라에서 레이캐스트 쏘기
+    public void ShootRaycast()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 1.5f, Color.red);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, 1.5f))
+        {
+            hitObject = hitInfo.collider.gameObject;
+        }
+        // 허공일 때
+        else
+        {
+            hitObject = null;
+        }
+
+        CheckObject();
+    }
+
+    public void CheckObject()
+    {
+        for(int i = 0; i < elevatorButtons.Length; i++)
+        {
+            if (hitObject == elevatorButtons[i])
+            {
+                if (preObject != hitObject.GetComponent<GetComponentScript>().mesh && preObject != null) // 전 오브젝트와 현재 오브젝트가 다를 때, 전 오브젝트 외곽선 끄기
+                {
+                    preObject.GetComponent<Outline>().enabled = false; // 외곽선 끄기
+                    preObject = null;
+                }
+
+                preObject = hitObject.GetComponent<GetComponentScript>().mesh;
+                preObject.GetComponent<Outline>().enabled = true; // 외곽선 켜기
+
+                // E키 입력 시
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    StartCoroutine(ElevatorCoroutine());
+                }
+                break;
+            }
+            else
+            {
+                mouseText.enabled = false; // 텍스트 없어짐
+
+                if (preObject != null)
+                {
+                    preObject.GetComponent<Outline>().enabled = false; // 외곽선 끄기
+                    preObject = null;
+                }
+            }
+        }
+    }
+
+    // 엘리베이터 버튼을 눌렀을 때
+    IEnumerator ElevatorCoroutine()
+    {
+        button = hitObject;
+
+        button.GetComponent<Collider>().enabled = false; // 버튼 콜라이더 비활성화
+        button.GetComponent<Outline>().enabled = false; // 버튼 외곽선 비활성화
+        button.GetComponent<NextElevatorPoint>().thisElevatorAnim.Play("Close");
+
+        elevator_Point_Player.transform.position = button.GetComponent<NextElevatorPoint>().thisPoint.transform.position; // elevator_Point_Player을 엘리베이터 포인트로 옮기고, 플레이어를 자식으로 넣어서
+        elevator_Point_Player.transform.rotation = button.GetComponent<NextElevatorPoint>().thisPoint.transform.rotation; // 2초 후에 다음 엘리베이터 포인트로 이동 후 자식 해제
+        player.transform.SetParent(elevator_Point_Player.transform);
+
+        yield return new WaitForSeconds(2f);
+
+        elevator_Point_Player.transform.position = button.GetComponent<NextElevatorPoint>().nextPoint.transform.position;
+        elevator_Point_Player.transform.rotation = button.GetComponent<NextElevatorPoint>().nextPoint.transform.rotation;
+
+        yield return new WaitForSeconds(2f);
+
+        button.GetComponent<NextElevatorPoint>().nextElevatorAnim.Play("Open");
+        player.transform.SetParent(Objects.transform);
+    }
+}
+    
---
+++ Assets/Scripts/04_Clown/NextElevatorPoint.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextElevatorPoint : MonoBehaviour
+{
+    public GameObject thisPoint;
+    public Animator thisElevatorAnim;
+
+    public GameObject nextPoint;
+    public Animator nextElevatorAnim;
+}
