a818c9c3b2394a5c05add02789815aab7bbc0c50
zazangkjw <zazangkjw@naver.com>
2024-05-21 19:41:08 +0900

    Shutter -> Flash 단어 변경 / 빌드

---
+++ Assets/Scripts/04_Clown/CircusFlash.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircusFlash : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] flashes;
+
+    bool[] flashesNum;
+
+    public float flashDelay = 1f; // 셔터 딜레이
+
+    public bool isFlashOn; // 셔터 온/오프
+
+
+
+
+    void Start()
+    {
+        flashes = new GameObject[transform.childCount];
+        flashesNum = new bool[flashes.Length];
+        isFlashOn = false;
+
+        // 자식 오브젝트(셔터)들 받아오기
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            flashes[i] = transform.GetChild(i).gameObject;
+            flashes[i].SetActive(false);
+        }
+
+        StartCoroutine(RandomCoroutine());
+    }
+
+
+
+
+    // 랜덤으로 셔터 고르기
+    IEnumerator RandomCoroutine()
+    {
+        while (true)
+        {
+            if (isFlashOn)
+            {
+                int rd = Random.Range(0, flashes.Length);
+
+                if (!flashesNum[rd])
+                {
+                    StartCoroutine(FlashCoroutine(rd));
+                    yield return new WaitForSeconds(flashDelay);
+                }
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+
+
+
+    // 셔터 효과
+    IEnumerator FlashCoroutine(int rd)
+    {
+        flashesNum[rd] = true;
+
+        flashes[rd].SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        flashes[rd].SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        flashes[rd].SetActive(true);
+        yield return new WaitForSeconds(0.01f);
+        flashes[rd].SetActive(false);
+
+        flashesNum[rd] = false;
+    }
+}
--- Assets/Scripts/04_Clown/ClownColliderTrigger.cs
+++ Assets/Scripts/04_Clown/ClownColliderTrigger.cs
@@ -5,64 +5,59 @@ using UnityEngine;
 public class ClownColliderTrigger : MonoBehaviour
 {
     [SerializeField]
-    Collider[] trigger_Chase; // 광대 추격 시작 트리거 콜라이더
+    Collider trigger_Chase; // 광대 추격 시작 트리거 콜라이더
 
     [SerializeField]
-    Collider[] trigger_SafeZone; // 안전 구역 트리거 콜라이더
+    Collider trigger_SafeZone; // 안전 구역 트리거 콜라이더
 
     [SerializeField]
     Clown_Chase clown_Chase;
 
-    void Start()
-    {
+    [SerializeField]
+    Collider trigger_CircusSuccess;
 
-    }
+    [SerializeField]
+    CircusFlash circusFlash;
 
-    void Update()
-    {
-        
-    }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
-        foreach (Collider col in trigger_SafeZone)
+        // 안전 구역 트리거
+        if (other == trigger_SafeZone)
         {
-            if (other == col)
-            {
-                clown_Chase.isSafe = true;
-            }
+            clown_Chase.isSafe = true;
+            circusFlash.isFlashOn = false;
+        }
+
+        // 서커스 도전 성공 트리거
+        if(other == trigger_CircusSuccess)
+        {
+            circusFlash.isFlashOn = true;
         }
     }
 
     // 추격 시작 트리거에 들어가면 광대 추격 시작
     private void OnTriggerStay(Collider other)
     {
-        foreach (Collider col in trigger_Chase)
+        if (other == trigger_Chase)
         {
-            if (other == col)
-            {
-                clown_Chase.isChasing = true;
-            }
+            clown_Chase.isChasing = true;
         }
     }
 
     // 추격 시작 트리거에서 나가면 광대 추격 종료
     private void OnTriggerExit(Collider other)
     {
-        foreach (Collider col in trigger_Chase)
+        if (other == trigger_Chase)
         {
-            if (other == col)
-            {
-                clown_Chase.isChasing = false;
-            }
+            clown_Chase.isChasing = false;
         }
 
-        foreach (Collider col in trigger_SafeZone)
+        if (other == trigger_SafeZone)
         {
-            if (other == col)
-            {
-                clown_Chase.isSafe = false;
-            }
+            clown_Chase.isSafe = false;
         }
     }
 }
